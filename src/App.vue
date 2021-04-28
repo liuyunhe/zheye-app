730acac383de3ad7c7c184c3f5d63c2904d6a4ee
liuyunhe <liuyunhe666@aliyun.com>
2021-04-28 21:42:44 +0800

    add validateInput

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,37 @@
 <template>
   <global-header :user="user"></global-header>
   <div class="container">
-    <column-list :list="list"></column-list>
+    <!-- <column-list :list="list"></column-list> -->
+    <form>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">邮箱地址</label>
+        <validate-input
+          type="text"
+          :rules="emailRules"
+          v-model="emailVal"
+          placeholder="请输入邮箱地址"
+        ></validate-input>
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">密码</label>
+        <validate-input
+          type="password"
+          :rules="passwordRules"
+          v-model="passwordVal"
+          placeholder="请输入密码"
+        ></validate-input>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import { defineComponent, reactive, ref } from "vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ColumnList, { ColumnProps } from "./components/ColumnList.vue";
 import GlobalHeader, { UserProps } from "./components/GlobalHeader.vue";
+import ValidateInput, { RulesProp } from "./components/ValidateInput.vue";
 const testData: ColumnProps[] = [
   {
     id: 1,
@@ -42,18 +64,32 @@ const testData: ColumnProps[] = [
 ];
 const currentUser: UserProps = {
   isLogin: true,
-  name: 'liuyunhe'
-}
+  name: "liuyunhe",
+};
 export default defineComponent({
   name: "App",
   components: {
-    ColumnList,
+    // ColumnList,
     GlobalHeader,
+    ValidateInput,
   },
   setup() {
+    const emailVal = ref("");
+    const emailRules: RulesProp = [
+      { type: "required", message: "电子邮箱地址不能为空" },
+      { type: "email", message: "请输入正确的电子邮箱格式" },
+    ];
+    const passwordVal = ref("");
+    const passwordRules: RulesProp = [
+      { type: "required", message: "密码不能为空" },
+    ];
     return {
       list: ref(testData),
       user: ref(currentUser),
+      emailVal,
+      emailRules,
+      passwordVal,
+      passwordRules,
     };
   },
 });
@@ -64,7 +100,6 @@ export default defineComponent({
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 </style>
