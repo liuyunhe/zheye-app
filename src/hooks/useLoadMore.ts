0eec1204a7f6586d31d4d99e14009774a296a38e
liuyunhe <liuyunhe666@aliyun.com>
2021-05-10 15:14:07 +0800

    useLoadMore函数增加cid参数

--- src/hooks/useLoadMore.ts
+++ src/hooks/useLoadMore.ts
@@ -4,16 +4,18 @@ import { useStore } from "vuex"
 
 interface LoadParams {
   currentPage: number;
-  pageSize: number
+  pageSize: number;
+  cid?:string
 }
 
 // eslint-disable-next-line
-const useLoadMore = (actionName: string, total: ComputedRef<number>, params: LoadParams = { currentPage: 2, pageSize: 5 }) => {
+const useLoadMore = (actionName: string, total: ComputedRef<number>, params: LoadParams = { currentPage: 2, pageSize: 5 },) => {
   const store = useStore()
   const currentPage = ref(params.currentPage)
   const requestParams = computed(() => ({
     currentPage: currentPage.value,
-    pageSize: params.pageSize
+    pageSize: params.pageSize,
+    cid:params.cid
   }))
   const loadMorePage = () => {
     store.dispatch(actionName, requestParams.value).then(() => {
