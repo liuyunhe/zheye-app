3ec46150f50c19396aba3d77861070bee3e5ed39
liuyunhe <liuyunhe666@aliyun.com>
2021-05-24 16:43:31 +0800

    样式优化

--- src/components/ValidateForm.vue
+++ src/components/ValidateForm.vue
@@ -1,7 +1,7 @@
 <template>
   <form class="validate-form-container">
     <slot name="default"></slot>
-    <div class="submit-area d-grid gap-2" @click.prevent="submitForm">
+    <div class="submit-area" @click.prevent="submitForm">
       <slot name="submit">
         <button type="submit" class="btn btn-primary">提交</button>
       </slot>
